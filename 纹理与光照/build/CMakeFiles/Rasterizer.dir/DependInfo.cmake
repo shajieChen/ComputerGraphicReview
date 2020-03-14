bd7671fbacb71c1a30cf60aea201862aa4823a8f
SJ cheng <sjcheng@SJdeMacBook-Pro.local>
2020-03-14 23:37:35 +0800

    纹理与光照

---
+++ 纹理与光照/build/CMakeFiles/Rasterizer.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/sjcheng/Desktop/图形学作业/PA3/Texture.cpp" "/Users/sjcheng/Desktop/图形学作业/PA3/build/CMakeFiles/Rasterizer.dir/Texture.cpp.o"
+  "/Users/sjcheng/Desktop/图形学作业/PA3/Triangle.cpp" "/Users/sjcheng/Desktop/图形学作业/PA3/build/CMakeFiles/Rasterizer.dir/Triangle.cpp.o"
+  "/Users/sjcheng/Desktop/图形学作业/PA3/main.cpp" "/Users/sjcheng/Desktop/图形学作业/PA3/build/CMakeFiles/Rasterizer.dir/main.cpp.o"
+  "/Users/sjcheng/Desktop/图形学作业/PA3/rasterizer.cpp" "/Users/sjcheng/Desktop/图形学作业/PA3/build/CMakeFiles/Rasterizer.dir/rasterizer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  ".././include"
+  "/usr/local/Cellar/opencv/4.2.0_3/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
